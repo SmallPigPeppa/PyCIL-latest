20540df3412236db028093963c5b3976fb97ed32
lwz <905328134@qq.com>
2023-12-02 21:28:40 +0800

    initial

--- run_semi.sh
+++ run_semi.sh
@@ -4,3 +4,4 @@ CUDA_VISIBLE_DEVICES=2 python main.py --config=./exps_semi/cifar100/5steps/memo_
 CUDA_VISIBLE_DEVICES=3 python main.py --config=./exps_semi/cifar100/5steps/rmm-foster_test.json &
 CUDA_VISIBLE_DEVICES=4 python main.py --config=./exps_semi/cifar100/5steps/simplecil_test.json &
 CUDA_VISIBLE_DEVICES=5 python main.py --config=./exps_semi/cifar100/5steps/ssre_test.json
+
