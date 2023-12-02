3f40d1d7038ca4b9d00af25bcd4ca037fe3a9db6
lwz <905328134@qq.com>
2023-12-03 02:34:45 +0800

    initial

--- run_semi_imagenet100.sh
+++ run_semi_imagenet100.sh
@@ -1,4 +1,4 @@
-#CUDA_VISIBLE_DEVICES=0 python main.py --config=./exps_semi/imagenet100/5steps/fetril.json &
+CUDA_VISIBLE_DEVICES=0 python main.py --config=./exps_semi/imagenet100/5steps/fetril.json
 #CUDA_VISIBLE_DEVICES=1 python main.py --config=./exps_semi/imagenet100/10steps/fetril.json &
 #CUDA_VISIBLE_DEVICES=2 python main.py --config=./exps_semi/imagenet100/5steps/foster.json &
 #CUDA_VISIBLE_DEVICES=3 python main.py --config=./exps_semi/imagenet100/10steps/foster.json &
@@ -8,5 +8,5 @@
 #CUDA_VISIBLE_DEVICES=7 python main.py --config=./exps_semi/imagenet100/10steps/rmm-foster.json &
 #CUDA_VISIBLE_DEVICES=0 python main.py --config=./exps_semi/imagenet100/5steps/simplecil.json &
 #CUDA_VISIBLE_DEVICES=1 python main.py --config=./exps_semi/imagenet100/10steps/simplecil.json &
-CUDA_VISIBLE_DEVICES=2 python main.py --config=./exps_semi/imagenet100/5steps/ssre.json &
-CUDA_VISIBLE_DEVICES=3 python main.py --config=./exps_semi/imagenet100/10steps/ssre.json
+#CUDA_VISIBLE_DEVICES=2 python main.py --config=./exps_semi/imagenet100/5steps/ssre.json &
+#CUDA_VISIBLE_DEVICES=3 python main.py --config=./exps_semi/imagenet100/10steps/ssre.json
