1bfd024bcecbe8ac0c59ce6851cc6835a6d1975b
lwz <905328134@qq.com>
2023-12-03 02:46:05 +0800

    initial

--- run_semi_cifar.sh
+++ run_semi_cifar.sh
@@ -5,7 +5,8 @@ CUDA_VISIBLE_DEVICES=3 python main.py --config=./exps_semi/cifar100/10steps/fost
 CUDA_VISIBLE_DEVICES=4 python main.py --config=./exps_semi/cifar100/5steps/memo.json &
 CUDA_VISIBLE_DEVICES=5 python main.py --config=./exps_semi/cifar100/10steps/memo.json &
 CUDA_VISIBLE_DEVICES=6 python main.py --config=./exps_semi/cifar100/5steps/rmm-foster.json &
-CUDA_VISIBLE_DEVICES=7 python main.py --config=./exps_semi/cifar100/10steps/rmm-foster.json &
+CUDA_VISIBLE_DEVICES=7 python main.py --config=./exps_semi/cifar100/10steps/rmm-foster.json
+
 CUDA_VISIBLE_DEVICES=0 python main.py --config=./exps_semi/cifar100/5steps/simplecil.json &
 CUDA_VISIBLE_DEVICES=1 python main.py --config=./exps_semi/cifar100/10steps/simplecil.json &
 CUDA_VISIBLE_DEVICES=2 python main.py --config=./exps_semi/cifar100/5steps/ssre.json &
