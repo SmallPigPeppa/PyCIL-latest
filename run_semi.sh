998fb099485658d2867d46f48da0186873ca96bf
lwz <905328134@qq.com>
2023-12-02 21:21:40 +0800

    initial

--- run_semi.sh
+++ run_semi.sh
@@ -1,6 +1,6 @@
-python main.py --config=./exps_semi/cifar100/5steps/fetril_test.json
-python main.py --config=./exps_semi/cifar100/5steps/fetril_test.json
-python main.py --config=./exps_semi/cifar100/5steps/fetril_test.json
-python main.py --config=./exps_semi/cifar100/5steps/fetril_test.json
-python main.py --config=./exps_semi/cifar100/5steps/fetril_test.json
-python main.py --config=./exps_semi/cifar100/5steps/fetril_test.json
+CUDA_VISIBLE_DEVICES=0 python main.py --config=./exps_semi/cifar100/5steps/fetril_test.json &
+CUDA_VISIBLE_DEVICES=1 python main.py --config=./exps_semi/cifar100/5steps/foster_test.json &
+CUDA_VISIBLE_DEVICES=2 python main.py --config=./exps_semi/cifar100/5steps/memo_test.json &
+CUDA_VISIBLE_DEVICES=3 python main.py --config=./exps_semi/cifar100/5steps/rmm-foster_test.json &
+CUDA_VISIBLE_DEVICES=4 python main.py --config=./exps_semi/cifar100/5steps/simplecil_test.json &
+CUDA_VISIBLE_DEVICES=5 python main.py --config=./exps_semi/cifar100/5steps/ssre_test.json
