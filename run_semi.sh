60d8e3cb0aff87cd01e9f6e5c1d55d5e5e95fcc3
lwz <905328134@qq.com>
2023-12-03 01:48:22 +0800

    initial

--- run_semi.sh
+++ run_semi.sh
@@ -1,7 +1,7 @@
-CUDA_VISIBLE_DEVICES=0 python main.py --config=./exps_semi/cifar100/5steps/fetril_test.json &
-CUDA_VISIBLE_DEVICES=1 python main.py --config=./exps_semi/cifar100/5steps/foster_test.json &
-CUDA_VISIBLE_DEVICES=2 python main.py --config=./exps_semi/cifar100/5steps/memo_test.json &
-CUDA_VISIBLE_DEVICES=3 python main.py --config=./exps_semi/cifar100/5steps/rmm-foster_test.json &
-CUDA_VISIBLE_DEVICES=4 python main.py --config=./exps_semi/cifar100/5steps/simplecil_test.json
+CUDA_VISIBLE_DEVICES=0 python main.py --config=./exps_semi/cifar100/5steps/fetril.json &
+CUDA_VISIBLE_DEVICES=1 python main.py --config=./exps_semi/cifar100/5steps/foster.json &
+CUDA_VISIBLE_DEVICES=2 python main.py --config=./exps_semi/cifar100/5steps/memo.json &
+CUDA_VISIBLE_DEVICES=3 python main.py --config=./exps_semi/cifar100/5steps/rmm-foster.json &
+CUDA_VISIBLE_DEVICES=4 python main.py --config=./exps_semi/cifar100/5steps/simplecil.json
 #CUDA_VISIBLE_DEVICES=5 python main.py --config=./exps_semi/cifar100/5steps/ssre_test.json
 
