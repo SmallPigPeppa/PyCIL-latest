38c803914101d008bb9c028ac08632a20ef20509
lwz <905328134@qq.com>
2023-12-02 22:39:27 +0800

    initial

--- run_semi.sh
+++ run_semi.sh
@@ -2,6 +2,6 @@ CUDA_VISIBLE_DEVICES=0 python main.py --config=./exps_semi/cifar100/5steps/fetri
 CUDA_VISIBLE_DEVICES=1 python main.py --config=./exps_semi/cifar100/5steps/foster_test.json &
 CUDA_VISIBLE_DEVICES=2 python main.py --config=./exps_semi/cifar100/5steps/memo_test.json &
 CUDA_VISIBLE_DEVICES=3 python main.py --config=./exps_semi/cifar100/5steps/rmm-foster_test.json &
-CUDA_VISIBLE_DEVICES=4 python main.py --config=./exps_semi/cifar100/5steps/simplecil_test.json &
-CUDA_VISIBLE_DEVICES=5 python main.py --config=./exps_semi/cifar100/5steps/ssre_test.json
+CUDA_VISIBLE_DEVICES=4 python main.py --config=./exps_semi/cifar100/5steps/simplecil_test.json
+#CUDA_VISIBLE_DEVICES=5 python main.py --config=./exps_semi/cifar100/5steps/ssre_test.json
 
