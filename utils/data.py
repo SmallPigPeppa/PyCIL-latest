01bcc75bebd3f6a6c310f26e815a3eec5032f48e
lwz <905328134@qq.com>
2023-12-04 03:52:23 +0800

    initial

--- utils/data.py
+++ utils/data.py
@@ -1,6 +1,7 @@
 import numpy as np
 from torchvision import datasets, transforms
 from utils.toolkit import split_images_labels
+from cub import Cub2011
 
 
 class iData(object):
@@ -123,3 +124,31 @@ class iImageNet100(iData):
 
         self.train_data, self.train_targets = split_images_labels(train_dset.imgs)
         self.test_data, self.test_targets = split_images_labels(test_dset.imgs)
+
+
+class iCUB200(iData):
+    use_path = True
+    train_trsf = [
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+    ]
+    test_trsf = [
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+    ]
+    common_trsf = [
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ]
+
+    class_order = np.arange(200).tolist()
+
+    def download_data(self):
+        train_dataset = Cub2011(root='/mnt/mmtech01/usr/liuwenzhuo/torch_ds', train=True, download=True)
+        test_dataset = Cub2011(root='/mnt/mmtech01/usr/liuwenzhuo/torch_ds', train=False, download=True)
+        self.train_data, self.train_targets = train_dataset.data, np.array(
+            train_dataset.targets
+        )
+        self.test_data, self.test_targets = test_dataset.data, np.array(
+            test_dataset.targets
+        )
