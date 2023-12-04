ffd60b90521d0bb25e5023e60237b291fbc3028a
lwz <905328134@qq.com>
2023-12-05 00:47:31 +0800

    initial

--- models/lwf.py
+++ models/lwf.py
@@ -15,7 +15,8 @@ init_epochs = 200
 init_lr = 0.1
 init_milestones = [60, 120, 160]
 init_lr_decay = 0.1
-init_weight_decay = 0.0005
+# init_weight_decay = 0.0005
+init_weight_decay = 2e-5
 
 
 epochs = 250
@@ -23,7 +24,8 @@ lrate = 0.1
 milestones = [60, 120, 180, 220]
 lrate_decay = 0.1
 batch_size = 128
-weight_decay = 2e-4
+# weight_decay = 2e-4
+weight_decay = 2e-5
 num_workers = 8
 T = 2
 lamda = 3
