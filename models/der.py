cd47e6871a082fe29f39f1f159d61d318e563e01
lwz <905328134@qq.com>
2023-12-05 00:37:56 +0800

    initial

--- models/der.py
+++ models/der.py
@@ -17,7 +17,8 @@ init_epoch = 200
 init_lr = 0.1
 init_milestones = [60, 120, 170]
 init_lr_decay = 0.1
-init_weight_decay = 5e-4
+# init_weight_decay = 5e-4
+init_weight_decay = 2e-5
 
 
 epochs = 170
@@ -25,7 +26,8 @@ lrate = 0.1
 milestones = [80, 120, 150]
 lrate_decay = 0.1
 batch_size = 128
-weight_decay = 2e-4
+# weight_decay = 2e-4
+weight_decay = 2e-5
 num_workers = 8
 T = 2
 
