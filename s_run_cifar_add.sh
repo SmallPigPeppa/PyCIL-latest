4013224facb1787eb7697a54f773e5cbbe017ef8
lwz <905328134@qq.com>
2023-12-05 10:01:38 +0800

    initial

--- s_run_cifar_add.sh
+++ s_run_cifar_add.sh
@@ -6,7 +6,7 @@ CUDA_VISIBLE_DEVICES=3 python main.py --config=./exps_semi_new/cifar100-byol/10s
 CUDA_VISIBLE_DEVICES=4 python main.py --config=./exps_semi_new/cifar100/5steps/il2a.json &
 CUDA_VISIBLE_DEVICES=5 python main.py --config=./exps_semi_new/cifar100/10steps/il2a.json &
 CUDA_VISIBLE_DEVICES=6 python main.py --config=./exps_semi_new/cifar100-byol/5steps/il2a.json &
-CUDA_VISIBLE_DEVICES=7 python main.py --config=./exps_semi_new/cifar100-byol/10steps/il2a.json &
+CUDA_VISIBLE_DEVICES=7 python main.py --config=./exps_semi_new/cifar100-byol/10steps/il2a.json
 
 CUDA_VISIBLE_DEVICES=0 python main.py --config=./exps_semi_new/cifar100/5steps/memo.json &
 CUDA_VISIBLE_DEVICES=1 python main.py --config=./exps_semi_new/cifar100/10steps/memo.json &
@@ -16,7 +16,7 @@ CUDA_VISIBLE_DEVICES=3 python main.py --config=./exps_semi_new/cifar100-byol/10s
 CUDA_VISIBLE_DEVICES=4 python main.py --config=./exps_semi_new/cifar100/5steps/der.json &
 CUDA_VISIBLE_DEVICES=5 python main.py --config=./exps_semi_new/cifar100/10steps/der.json &
 CUDA_VISIBLE_DEVICES=6 python main.py --config=./exps_semi_new/cifar100-byol/5steps/der.json &
-CUDA_VISIBLE_DEVICES=7 python main.py --config=./exps_semi_new/cifar100-byol/10steps/der.json &
+CUDA_VISIBLE_DEVICES=7 python main.py --config=./exps_semi_new/cifar100-byol/10steps/der.json
 
 CUDA_VISIBLE_DEVICES=0 python main.py --config=./exps_semi_new/cifar100/5steps/pass.json &
 CUDA_VISIBLE_DEVICES=1 python main.py --config=./exps_semi_new/cifar100/10steps/pass.json &
