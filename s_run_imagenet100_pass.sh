270826341209502f6fce8149e9c6120db4da2a3b
lwz <905328134@qq.com>
2023-12-05 22:30:02 +0800

    initial

--- s_run_imagenet100_pass.sh
+++ s_run_imagenet100_pass.sh
@@ -44,14 +44,14 @@
 
 # add3
 CUDA_VISIBLE_DEVICES=0 python main.py --config=./exps_semi_new/imagenet100/5steps/pass.json &
-CUDA_VISIBLE_DEVICES=1 python main.py --config=./exps_semi_new/imagenet100/10steps/pass.json &
-CUDA_VISIBLE_DEVICES=2 python main.py --config=./exps_semi_new/imagenet100-byol/5steps/pass.json &
-CUDA_VISIBLE_DEVICES=3 python main.py --config=./exps_semi_new/imagenet100-byol/10steps/pass.json &
-
-CUDA_VISIBLE_DEVICES=4 python main.py --config=./exps_semi_new/imagenet100/5steps/il2a.json &
-CUDA_VISIBLE_DEVICES=5 python main.py --config=./exps_semi_new/imagenet100/10steps/il2a.json &
-CUDA_VISIBLE_DEVICES=6 python main.py --config=./exps_semi_new/imagenet100-byol/5steps/il2a.json &
-CUDA_VISIBLE_DEVICES=7 python main.py --config=./exps_semi_new/imagenet100-byol/10steps/il2a.json
+#CUDA_VISIBLE_DEVICES=1 python main.py --config=./exps_semi_new/imagenet100/10steps/pass.json &
+#CUDA_VISIBLE_DEVICES=2 python main.py --config=./exps_semi_new/imagenet100-byol/5steps/pass.json &
+#CUDA_VISIBLE_DEVICES=3 python main.py --config=./exps_semi_new/imagenet100-byol/10steps/pass.json &
+#
+#CUDA_VISIBLE_DEVICES=4 python main.py --config=./exps_semi_new/imagenet100/5steps/il2a.json &
+#CUDA_VISIBLE_DEVICES=5 python main.py --config=./exps_semi_new/imagenet100/10steps/il2a.json &
+#CUDA_VISIBLE_DEVICES=6 python main.py --config=./exps_semi_new/imagenet100-byol/5steps/il2a.json &
+#CUDA_VISIBLE_DEVICES=7 python main.py --config=./exps_semi_new/imagenet100-byol/10steps/il2a.json
 
 
 
