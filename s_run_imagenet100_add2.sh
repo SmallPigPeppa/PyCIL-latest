73791a68f6abf49b0a37701ce609e7e85888e450
lwz <905328134@qq.com>
2023-12-05 10:29:06 +0800

    initial

--- s_run_imagenet100_add2.sh
+++ s_run_imagenet100_add2.sh
@@ -1,4 +1,15 @@
-## add1
+### add1
+#
+#CUDA_VISIBLE_DEVICES=0 python main.py --config=./exps_semi_new/imagenet100/5steps/beef.json &
+#CUDA_VISIBLE_DEVICES=1 python main.py --config=./exps_semi_new/imagenet100/10steps/beef.json &
+#CUDA_VISIBLE_DEVICES=2 python main.py --config=./exps_semi_new/imagenet100-byol/5steps/beef.json &
+#CUDA_VISIBLE_DEVICES=3 python main.py --config=./exps_semi_new/imagenet100-byol/10steps/beef.json &
+#
+#CUDA_VISIBLE_DEVICES=4 python main.py --config=./exps_semi_new/imagenet100/5steps/der.json &
+#CUDA_VISIBLE_DEVICES=5 python main.py --config=./exps_semi_new/imagenet100/10steps/der.json &
+#CUDA_VISIBLE_DEVICES=6 python main.py --config=./exps_semi_new/imagenet100-byol/5steps/der.json &
+#CUDA_VISIBLE_DEVICES=7 python main.py --config=./exps_semi_new/imagenet100-byol/10steps/der.json
+#
 #CUDA_VISIBLE_DEVICES=0 python main.py --config=./exps_semi_new/imagenet100/5steps/foster.json &
 #CUDA_VISIBLE_DEVICES=1 python main.py --config=./exps_semi_new/imagenet100/10steps/foster.json &
 #CUDA_VISIBLE_DEVICES=2 python main.py --config=./exps_semi_new/imagenet100-byol/5steps/foster.json &
@@ -8,16 +19,8 @@
 #CUDA_VISIBLE_DEVICES=5 python main.py --config=./exps_semi_new/imagenet100/10steps/ssre.json &
 #CUDA_VISIBLE_DEVICES=6 python main.py --config=./exps_semi_new/imagenet100-byol/5steps/ssre.json &
 #CUDA_VISIBLE_DEVICES=7 python main.py --config=./exps_semi_new/imagenet100-byol/10steps/ssre.json
-#
-#CUDA_VISIBLE_DEVICES=0 python main.py --config=./exps_semi_new/imagenet100/5steps/memo.json &
-#CUDA_VISIBLE_DEVICES=1 python main.py --config=./exps_semi_new/imagenet100/10steps/memo.json &
-#CUDA_VISIBLE_DEVICES=2 python main.py --config=./exps_semi_new/imagenet100-byol/5steps/memo.json &
-#CUDA_VISIBLE_DEVICES=3 python main.py --config=./exps_semi_new/imagenet100-byol/10steps/memo.json &
-#
-#CUDA_VISIBLE_DEVICES=4 python main.py --config=./exps_semi_new/imagenet100/5steps/der.json &
-#CUDA_VISIBLE_DEVICES=5 python main.py --config=./exps_semi_new/imagenet100/10steps/der.json &
-#CUDA_VISIBLE_DEVICES=6 python main.py --config=./exps_semi_new/imagenet100-byol/5steps/der.json &
-#CUDA_VISIBLE_DEVICES=7 python main.py --config=./exps_semi_new/imagenet100-byol/10steps/der.json
+
+
 
 
 
@@ -25,20 +28,22 @@
 
 # add2
 
-#CUDA_VISIBLE_DEVICES=0 python main.py --config=./exps_semi_new/imagenet100/5steps/icarl.json &
-#CUDA_VISIBLE_DEVICES=1 python main.py --config=./exps_semi_new/imagenet100/10steps/icarl.json &
-#CUDA_VISIBLE_DEVICES=2 python main.py --config=./exps_semi_new/imagenet100-byol/5steps/icarl.json &
-#CUDA_VISIBLE_DEVICES=3 python main.py --config=./exps_semi_new/imagenet100-byol/10steps/icarl.json &
-#
-#CUDA_VISIBLE_DEVICES=4 python main.py --config=./exps_semi_new/imagenet100/5steps/lwf.json &
-#CUDA_VISIBLE_DEVICES=5 python main.py --config=./exps_semi_new/imagenet100/10steps/lwf.json &
-#CUDA_VISIBLE_DEVICES=6 python main.py --config=./exps_semi_new/imagenet100-byol/5steps/lwf.json &
-#CUDA_VISIBLE_DEVICES=7 python main.py --config=./exps_semi_new/imagenet100-byol/10steps/lwf.json
 
-CUDA_VISIBLE_DEVICES=0 python main.py --config=./exps_semi_new/imagenet100/5steps/beef.json &
-CUDA_VISIBLE_DEVICES=1 python main.py --config=./exps_semi_new/imagenet100/10steps/beef.json &
-CUDA_VISIBLE_DEVICES=2 python main.py --config=./exps_semi_new/imagenet100-byol/5steps/beef.json &
-CUDA_VISIBLE_DEVICES=3 python main.py --config=./exps_semi_new/imagenet100-byol/10steps/beef.json &
+CUDA_VISIBLE_DEVICES=0 python main.py --config=./exps_semi_new/imagenet100/5steps/memo.json &
+CUDA_VISIBLE_DEVICES=1 python main.py --config=./exps_semi_new/imagenet100/10steps/memo.json &
+CUDA_VISIBLE_DEVICES=2 python main.py --config=./exps_semi_new/imagenet100-byol/5steps/memo.json &
+CUDA_VISIBLE_DEVICES=3 python main.py --config=./exps_semi_new/imagenet100-byol/10steps/memo.json &
+
+CUDA_VISIBLE_DEVICES=4 python main.py --config=./exps_semi_new/imagenet100/5steps/lwf.json &
+CUDA_VISIBLE_DEVICES=5 python main.py --config=./exps_semi_new/imagenet100/10steps/lwf.json &
+CUDA_VISIBLE_DEVICES=6 python main.py --config=./exps_semi_new/imagenet100-byol/5steps/lwf.json &
+CUDA_VISIBLE_DEVICES=7 python main.py --config=./exps_semi_new/imagenet100-byol/10steps/lwf.json
+
+CUDA_VISIBLE_DEVICES=0 python main.py --config=./exps_semi_new/imagenet100/5steps/icarl.json &
+CUDA_VISIBLE_DEVICES=1 python main.py --config=./exps_semi_new/imagenet100/10steps/icarl.json &
+CUDA_VISIBLE_DEVICES=2 python main.py --config=./exps_semi_new/imagenet100-byol/5steps/icarl.json &
+CUDA_VISIBLE_DEVICES=3 python main.py --config=./exps_semi_new/imagenet100-byol/10steps/icarl.json &
+
 
 CUDA_VISIBLE_DEVICES=4 python main.py --config=./exps_semi_new/imagenet100/5steps/il2a.json &
 CUDA_VISIBLE_DEVICES=5 python main.py --config=./exps_semi_new/imagenet100/10steps/il2a.json &
