1c15c7e438d5c98e569a065dc87da2534791fb5d
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-12-06 15:26:49 +0800

    initial

--- s_run_imagenet100_add3.sh
+++ s_run_imagenet100_add3.sh
@@ -55,8 +55,8 @@
 
 #
 CUDA_VISIBLE_DEVICES=0,1,2,3 /root/miniconda3/envs/torch/bin/python main.py --config=./exps_semi_new/imagenet100/5steps/pass.json &
-CUDA_VISIBLE_DEVICES=4,5,6,7 /root/miniconda3/envs/torch/bin/python main.py --config=./exps_semi_new/imagenet100-byol/5steps/pass.json
-CUDA_VISIBLE_DEVICES=0,1,2,3 /root/miniconda3/envs/torch/bin/python main.py --config=./exps_semi_new/imagenet100/10steps/pass.json&
+CUDA_VISIBLE_DEVICES=4,5,6,7 /root/miniconda3/envs/torch/bin/python main.py --config=./exps_semi_new/imagenet100/10steps/pass.json
+CUDA_VISIBLE_DEVICES=0,1,2,3 /root/miniconda3/envs/torch/bin/python main.py --config=./exps_semi_new/imagenet100-byol/5steps/pass.json &
 CUDA_VISIBLE_DEVICES=4,5,6,7 /root/miniconda3/envs/torch/bin/python main.py --config=./exps_semi_new/imagenet100-byol/10steps/pass.json
 
 #
