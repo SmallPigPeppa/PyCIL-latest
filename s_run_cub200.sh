d5aa8f015d87a1e0a455e0d2e48aeca5bd54f9b3
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-12-11 01:46:11 +0800

    initial

--- s_run_cub200.sh
+++ s_run_cub200.sh
@@ -26,6 +26,7 @@ CUDA_VISIBLE_DEVICES=7 python main.py --config=./exps_semi_new/cub200-byol/10ste
 CUDA_VISIBLE_DEVICES=0 python main.py --config=./exps_semi_new/cub200-byol/5steps/fetril.json &
 CUDA_VISIBLE_DEVICES=1 python main.py --config=./exps_semi_new/cub200-byol/10steps/fetril.json
 
+
 #CUDA_VISIBLE_DEVICES=0,1,2,3 /root/miniconda3/envs/torch/bin/python main.py --config=./exps_semi_new/cub200/5steps/pass.json &
 #CUDA_VISIBLE_DEVICES=4,5,6,7 /root/miniconda3/envs/torch/bin/python main.py --config=./exps_semi_new/cub200/10steps/pass.json
 #CUDA_VISIBLE_DEVICES=0,1,2,3 /root/miniconda3/envs/torch/bin/python main.py --config=./exps_semi_new/cub200-byol/5steps/pass.json &
