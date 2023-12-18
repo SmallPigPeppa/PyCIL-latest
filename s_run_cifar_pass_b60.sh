093b588a1de11cc195a2bcc34b22968eddb34125
lwz <905328134@qq.com>
2023-12-18 14:04:29 +0800

    initial

--- s_run_cifar_pass_b60.sh
+++ s_run_cifar_pass_b60.sh
@@ -1,3 +1,4 @@
-CUDA_VISIBLE_DEVICES=4 python main.py --config=./exps_semi_new/cifar100-byol/base-60/pass.json &
-CUDA_VISIBLE_DEVICES=5 python main.py --config=./exps_semi_new/cifar100-byol/base-60/der.json
+#CUDA_VISIBLE_DEVICES=4 python main.py --config=./exps_semi_new/cifar100-byol/base-60/pass.json &
+#CUDA_VISIBLE_DEVICES=5 python main.py --config=./exps_semi_new/cifar100-byol/base-60/der.json
+CUDA_VISIBLE_DEVICES=1 python main.py --config=./exps_semi_new/cifar100-byol/base-60/fetril.json
 
