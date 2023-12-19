92d5e04c745bab0a8dca6f1892758e1ad713779d
lwz <905328134@qq.com>
2023-12-19 20:14:44 +0800

    initial

--- s_run_mini_add1.sh
+++
@@ -1,3 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python main.py --config=./exps_semi_new/mini-byol/10steps/memo.json
-#CUDA_VISIBLE_DEVICES=1 python main.py --config=./exps_semi_new/mini-byol/5steps/der.json &
-#CUDA_VISIBLE_DEVICES=2 python main.py --config=./exps_semi_new/mini-byol/10steps/der.json
---
+++ s_run_mini_pass.sh
@@ -0,0 +1,5 @@
+python main.py --config=./exps_semi_new/mini-byol/5steps/pass.json
+python main.py --config=./exps_semi_new/mini-byol/10steps/pass.json
+
+
+
